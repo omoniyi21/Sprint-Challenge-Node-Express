aaf6a575f0d27c94216e01021343b9b2dede9b37
Christopher Atoki <christopher.atoki@yahoo.com>
2018-04-06 17:08:57 -0400

    Added some comments on a few things to fix up a bit

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
-const helmet = require('morgan');
+const helmet = require('morgan'); // should be helmet
 const morgan = require('morgan');
 
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
-const actionModel = require("");
-const projectModel = require("");
+const actionModel = require(""); // (./actions/actionModel)
+const projectModel = require(""); // (./projects/projectModel)
 
 
 
-server.use(logger);
+server.use(logger); // this will be undefined
 server.use(morgan('dev'));
-server.use(helemt());
+server.use(helemt()); // typo
 server.use(express.json());
 
+/*
+* You don't need these two
+*/
 server.use("");
 server.use("")
 
